a21f31a0d9062c061097c1ac982c9b59f492ebf9
mrwilliambarenga <williambarenga@gmail.com>
2024-03-23 20:06:44 +0000

    Change routing to use HashRouter PROPERLY

--- narr-ai-te/src/App.js
+++ narr-ai-te/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ImageProvider } from "./contexts/ImageContext";
 import { CanvasProvider } from "./contexts/CanvasContext";
 import "./App.css";
