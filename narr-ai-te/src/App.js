3f6827d21efc5adaa4a9959ecafb686d9e685c6e
mrwilliambarenga <williambarenga@gmail.com>
2024-03-23 20:00:40 +0000

    Change routing to use HashRouter

--- narr-ai-te/src/App.js
+++ narr-ai-te/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Routes, Route } from "react-router-dom";
 import { ImageProvider } from "./contexts/ImageContext";
 import { CanvasProvider } from "./contexts/CanvasContext";
 import "./App.css";
@@ -15,11 +15,11 @@ function App() {
       <ImageProvider>
         <CanvasProvider>
           <Routes>
-            <Route path="/narr-ai-te" element={<Home />} />
-            <Route path="/narr-ai-te/view-comic" element={<ViewComic />} />
-            <Route path="/narr-ai-te/whiteboard" element={<Whiteboard />} />
-            <Route path="/narr-ai-te/whiteboard/:id" element={<Whiteboard />} />
-            <Route path="/narr-ai-te/generation" element={<GenerateImage />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/view-comic" element={<ViewComic />} />
+            <Route path="/whiteboard" element={<Whiteboard />} />
+            <Route path="/whiteboard/:id" element={<Whiteboard />} />
+            <Route path="/generation" element={<GenerateImage />} />
           </Routes>
         </CanvasProvider>
       </ImageProvider>
